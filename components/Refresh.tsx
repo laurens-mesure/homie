b85a0d08fc7d6e333955e4654778767a09ad3f7f
Laurens Mesure <2000laurens@gmail.com>
2023-01-31 00:14:48 +0100

    Add new colours

--- components/Refresh.tsx
+++ components/Refresh.tsx
@@ -9,7 +9,7 @@ export function Refresh() {
 
   return (
     <button
-      className="sticky top-5 right-5 bottom-5 ml-auto self-start rounded-md bg-sky-900 p-5"
+      className="sticky top-5 right-5 bottom-5 ml-auto self-start rounded-md bg-indigo-800 p-5"
       onClick={() => {
         reScan();
         setStore((prev) => ({
